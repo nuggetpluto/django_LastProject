9243a77685d647ff1ea0aeae2e50d0c2f9b5605f
nuggetpluto <korshikov1501@gmail.com>
2024-10-21 19:50:19 +0300

    Сайт администрирования

--- myshop/task1/admin.py
+++ myshop/task1/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from task1.models import Buyer, Game
 
-# Register your models here.
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'balance', 'age',)
+    search_fields = ('name', 'age',)
+    list_filter = ('balance',)  # Фильтр по балансу
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'cost', 'size', 'description', 'age_limited',)
+    list_filter = ('title', 'cost', 'age_limited')
+    fieldsets = (
+        ('main_info', {
+            'fields': ('title', 'cost', 'age_limited')
+        }),
+        ('additional_info', {
+            'fields': ('size', 'description')
+        }),
+    )
